9d8e7be4b19e143d14aadef8ff52c8090bcd31d1
a.joshi <a.joshi@levi9.com>
2017-03-22 14:17:59 +0200

    initial commit

---
+++ src/main/java/edu/arturjoshi/SpringIntegrationLearnApplication.java
@@ -0,0 +1,12 @@
+package edu.arturjoshi;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringIntegrationLearnApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringIntegrationLearnApplication.class, args);
+	}
+}
